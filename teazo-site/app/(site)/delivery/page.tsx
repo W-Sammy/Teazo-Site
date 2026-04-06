ba74eb3b4551be0dda01ef01f342dc32c4f551dd
Kaylina <kaylikwong@gmail.com>
2026-04-06 00:53:41 -0700

    Added more content to Delivery page and cleaned syntax for nav bar and button components

--- teazo-site/app/(site)/delivery/page.tsx
+++ teazo-site/app/(site)/delivery/page.tsx
@@ -11,6 +11,17 @@ const cabinSketch = Cabin_Sketch({
     weight: ['400'] // normal
 });
 
+// extra bold montserrat
+const boldMontserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['800'] // extra bold
+});
+
+const mediumMontserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['500']
+});
+
 // need to setup montserrat
 
 export default function Delivery() {
@@ -40,12 +51,17 @@ export default function Delivery() {
         </div>
 
         {/* description text */}
-        <div className="absolute z-20 w-full flex items-center justify-center pt-150 gap-12">
-            //
+        <div className="absolute z-20 w-full flex flex-col items-center justify-center pt-130 gap-1">
+            <h1 className={`${mediumMontserrat.className} text-[32px] text-black tracking-[0.1em]`}>
+                HUNGRY AT HOME?
+            </h1>
+            <h1 className={`${boldMontserrat.className} text-[36px] text-black tracking-[0.1em]`}>
+                WE DELIVER.
+            </h1>
         </div>
         
         {/* buttons */}
-        <div className="absolute z-20 w-full flex items-center justify-center pt-150 gap-12">
+        <div className="absolute z-20 w-full flex items-center justify-center pt-175 gap-12">
             <GeneralButton text="UBER EATS" href="https://www.ubereats.com/store/teazo/HmB7kkvSQdeWw6qSClzgzg?srsltid=AfmBOoranl_YtSY-qug2w6ZzmFcwawnUN1t6RJMvTnqq32BwwVXubRgr" />
             <GeneralButton text="DOORDASH" href="https://www.doordash.com/en/store/teazo-san-francisco-849601/1213761/?srsltid=AfmBOortGz8HB9dVSbrcnGxXWHRoalBu_ObBQ_Fv-r0SRKiFrYvWQawu" />
             <GeneralButton text="POSTMATES" href="https://postmates.com/store/teazo/HmB7kkvSQdeWw6qSClzgzg" />
--- teazo-site/app/components/general-button.tsx
+++ teazo-site/app/components/general-button.tsx
@@ -16,8 +16,13 @@ interface Editables {
 // button.
 export default function GeneralButton({ text, href }: Editables) {
     return (
-        <Link href={href} className="block w-[213px]">
-            <button className={`${montserrat.className} w-[213px] h-[70px] bg-black text-white text-18px flex items-center justify-center hover:bg-[#FFBDC7] transition-colors`}>
+        <Link 
+            href={href} 
+            className="block w-[213px]"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <button className={`${montserrat.className} w-[213px] h-[70px] bg-black text-white text-[18px] flex items-center justify-center hover:bg-[#FFBDC7] transition-colors tracking-[0.05em]`}>
                 {text}
             </button>
         </Link>
