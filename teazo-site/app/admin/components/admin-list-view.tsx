94a090217a7663e7384d590a574d12757c9947db
phtevesn <steveninouye@csus.edu>
2026-04-11 12:11:16 -0700

    Added: edit and delete buttons, function handlers are not created.

--- teazo-site/app/admin/components/admin-list-view.tsx
+++ teazo-site/app/admin/components/admin-list-view.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 type Row = Partial<Record<string, string | string[] | number | number[] | boolean | null>>;
 
 export default function ListView({items} :  {items: Row[]}){
@@ -9,6 +9,7 @@ export default function ListView({items} :  {items: Row[]}){
 
   /* gets the different keys of the json array */
   const keys = Array.from(new Set(items.flatMap((item) => Object.keys(item))))
+  //need to implement function to ensure there exist an id column
 
   function checkCell(value: unknown) {
     if (value == null) return "N/A";
@@ -21,12 +22,20 @@ export default function ListView({items} :  {items: Row[]}){
     return str.length > 20 ? str.slice(0, 20) + "…" : str;
   }
 
+  function editHandler(item: Row){
+    console.log("edit");
+  }
+  function deleteHandler(item: Row){
+    console.log("delete")
+  }
+
   return (
     <div className="w-full">
       <table className = "w-full border-collapse">
         {/*creates the column names*/}
         <thead>
           <tr className="border-b border-[#dbb082]">
+            <th className="w-6"></th> {/* delete column */}
             {keys.map((key) => (
               <th key = {key} className = "pr-1 py-2 text-left">
                 <div title={key}>
@@ -42,7 +51,23 @@ export default function ListView({items} :  {items: Row[]}){
         {/*prints the row dakines*/}
         <tbody>
           {items.map((item, row_i) => (
-            <tr key={row_i} className= "border-b border-[#dbb082]/50">
+            <tr
+              key={row_i}
+              onClick={() => editHandler(item)}
+              className="border-b border-[#dbb082]/50 cursor-pointer hover:bg-[#dbb082]/25"
+            >
+              {/* delete cell */}
+              <td className="pr-6 py-2">
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation(); //prevents row click
+                    deleteHandler(item);
+                  }}
+                  className="text-red-500 hover:text-red-700 cursor-pointer"
+                >
+                  ✕
+                </button>
+              </td>
               {keys.map((key)=>(
                 <td key={key} className=" pr-1 py-2">
                   <div title={String(item[key])}>
