a03929f88db2874aad63efbfd0e44995f6077ea9
Kaylina <kaylikwong@gmail.com>
2026-04-19 16:48:03 -0700

    Create Subtitle component, hardlock button and subtitle components to uppercase

---
+++ teazo-site/app/(site)/components/sub-title.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { Cabin_Sketch } from "next/font/google"; // import fonts
+
+// setup fonts
+const cabinSketch = Cabin_Sketch({
+    subsets: ['latin'], // reduce amount of importing to just letters, nums
+    weight: ['400'] // normal
+});
+
+interface Editables {
+    text: string;
+}
+
+export default function Subtitle({ text }: Editables) {
+    return (
+        <div className="relative inline-block">
+            {/* pink scribble */}
+            <Image
+                src="/pink_scribble.png"
+                alt="Pink Silhouette of a Chalk Scribble"
+                aria-hidden="true"
+                width={582}
+                height={104}
+                className="w-[355px] h-[63px] md:w-[582px] md:h-[104px]"
+                priority
+            />
+
+            {/* text */}
+            <div className="absolute inset-0 flex items-center justify-center">
+                {/* locked to uppercase */}
+                <h2 className={`${cabinSketch.className} text-[48px] md:text-[64px] text-black uppercase pt-4`}>
+                    {text}
+                </h2>
+            </div>
+        </div>
+        
+    );
+}
