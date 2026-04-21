8df673da217e62caa7ad1361fe4967f6bdb2a9b6
phtevesn <steveninouye@csus.edu>
2026-04-21 15:33:50 -0700

    Edited: flushed nav bar to the top

--- teazo-site/app/admin/layout.tsx
+++ teazo-site/app/admin/layout.tsx
@@ -9,9 +9,9 @@ export default function AdminLayout({
 }){
   return (
     <div className="flex h-full bg-white text-black">
-      <aside className="sticky top-16 h-[100vh] ">
-        <Sidebar />
-      </aside>
+      <aside className="sticky top-0 h-screen">
+  <Sidebar />
+</aside>
       
       <main className="flex-1 overflow-y-auto p-6">
         {children}
